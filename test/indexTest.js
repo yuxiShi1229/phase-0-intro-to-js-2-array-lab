8444d7f0ce9368acd3a9c9d4f9d0a7df74957f95
yuxi Shi <ys3599@tc.columbia.edu>
2024-05-29 23:52:14 -0400

    complete arrays lab

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,4 +1,42 @@
 require ( './helpers.js' );
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+  return cats;
+}
+
+function destructivelyPrependCat(name) {
+  cats.unshift(name);
+  return cats;
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+  return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+  return cats;
+}
+
+function appendCat(name) {
+  return [...cats, name];
+}
+
+function prependCat(name) {
+  return [name, ...cats];
+}
+
+function removeLastCat() {
+  return cats.slice(0, -1);
+}
+
+function removeFirstCat() {
+  return cats.slice(1);
+}
+
 
 describe('index.js', function () {
   describe('cats', function () {
